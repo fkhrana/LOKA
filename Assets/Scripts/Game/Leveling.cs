a6ec6eb0c617ea883d24853ab7b21af9236f20d8
dazaissi <naylalzahra09@gmail.com>
2026-07-15 19:39:53 +0700

    updated scene leveling
    benerin button leveling, ui, dan fix bugs

--- Assets/Scripts/Game/Leveling.cs
+++ Assets/Scripts/Game/Leveling.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Leveling : MonoBehaviour
 {
@@ -7,6 +8,66 @@ public class Leveling : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+    public void OpenLevel1()
+    {
+        SceneManager.LoadScene("Scratch");
+    }
+
+    public void OpenLevel2()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void OpenLevel3()
+    {
+        SceneManager.LoadScene("Win");
+    }
+
+    public GameObject scrolbar;
+    float scroll_pos = 0;
+    float[] pos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        pos = new float[transform.childCount];
+
+        if (pos.Length <= 1 || scrolbar == null)
+        {
+            return;
+        }
+
+        float distance = 1f / (pos.Length - 1f);
+
+        for (int i = 0; i < pos.Length; i++)
+        {
+            pos[i] = distance * i;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            scroll_pos = scrolbar.GetComponent<Scrollbar>().value;
+        }
+        else
+        {
+            for (int i = 0; i < pos.Length; i++)
+            {
+                if (scroll_pos < pos[i] + (distance / 2f) &&
+                    scroll_pos > pos[i] - (distance / 2f))
+                {
+                    scrolbar.GetComponent<Scrollbar>().value =
+                        Mathf.Lerp(
+                            scrolbar.GetComponent<Scrollbar>().value,
+                            pos[i],
+                            0.1f
+                        );
+                }
+            }
+        }
+    }
 }
-
-
